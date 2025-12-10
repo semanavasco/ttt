639c9b5b7b92866c24ad4c9626acf9e8f2420c75
semanavasco <semanavasco@gmail.com>
2025-12-10 07:55:32 +0100

    First ui

--- src/app/events.rs
+++ src/app/events.rs
@@ -1,7 +1,19 @@
 use std::io;
 
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+
 use crate::app::state::State;
 
-pub fn handle_events(_state: &mut State) -> io::Result<()> {
+pub fn handle_events(state: &mut State) -> io::Result<()> {
+    if let Event::Key(key) = event::read()? {
+        if key.kind == KeyEventKind::Release {
+            return Ok(());
+        }
+
+        match key.code {
+            KeyCode::Esc => state.exit = true,
+            _ => {}
+        }
+    }
     Ok(())
 }
--- src/app/ui.rs
+++ src/app/ui.rs
@@ -1,7 +1,32 @@
-use ratatui::Frame;
+use ratatui::{
+    Frame,
+    layout::{Constraint, Layout},
+    symbols,
+    text::Line,
+    widgets::{Block, BorderType, Borders, Paragraph},
+};
 
 use crate::app::state::State;
 
-pub fn draw(_frame: &mut Frame, _state: &State) {
-    todo!("Implement drawing method");
+pub fn draw(frame: &mut Frame, _state: &State) {
+    let layout = Layout::vertical([Constraint::Min(10), Constraint::Length(3)]).split(frame.area());
+
+    let main_block = Block::new()
+        .borders(Borders::TOP | Borders::LEFT | Borders::RIGHT)
+        .border_type(BorderType::Rounded)
+        .title(Line::from(" TTT ").centered());
+
+    let footer_block = Block::new()
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded)
+        .border_set(symbols::border::Set {
+            top_left: symbols::line::NORMAL.vertical_right,
+            top_right: symbols::line::NORMAL.vertical_left,
+            ..symbols::border::ROUNDED
+        });
+
+    let footer = Paragraph::new(" Quit (ESC)").block(footer_block);
+
+    frame.render_widget(main_block, layout[0]);
+    frame.render_widget(footer, layout[1]);
 }
